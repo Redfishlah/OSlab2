de0352cb5b0f0013650a058f9460a47da7fa82a7
JesWWW <149551099+JesWWW@users.noreply.github.com>
2025-04-21 19:59:43 +0800

    Update synch.h

--- lab02/pintos-main_done/threads/synch.h
+++ lab02/pintos-main_done/threads/synch.h
@@ -19,10 +19,14 @@ void sema_self_test (void);
 
 /* Lock. */
 struct lock 
-  {
-    struct thread *holder;      /* Thread holding lock (for debugging). */
-    struct semaphore semaphore; /* Binary semaphore controlling access. */
-  };
+{
+  struct thread *holder;      /* Thread holding lock (for debugging). */
+  struct semaphore semaphore; /* Binary semaphore controlling access. */
+
+  /*Lab02:Priority test*/
+  int max_priority;                    /* Max priority among threads waiting for this lock */
+  struct list_elem elem;              /* List element for thread->locks */
+};
 
 void lock_init (struct lock *);
 void lock_acquire (struct lock *);
